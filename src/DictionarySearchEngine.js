882c65c247173e3dbc5103f7e1c2098c4eadb53f
Atefeh <at.mirzabeigi@gmail.com>
2021-10-23 19:36:07 +0330

    add api and call

--- src/DictionarySearchEngine.js
+++ src/DictionarySearchEngine.js
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
 import "./SearchEngine.css";
+import axios from "axios";
+
 export default function DictionarySearchEngine() {
   const [searchWord, setSearchWord] = useState(null);
+  const [newWord, setNewWord] = useState({});
+
+  function handleResponse(res) {
+    console.log(res.data);
+    setNewWord({
+      meaning: res.data.meaning,
+    });
+  }
   function handleSubmit(event) {
     event.preventDefault();
     console.log(searchWord);
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleSearch(event) {
     setSearchWord(event.target.value);
   }
+
   return (
     <div className="SearchEngine container-fluid">
-      <h1 className="search__heading text-center mb-0 mt-3">
-        Search Your Word
-      </h1>
       <div className="search">
+        <h1 className="search__heading text-center mb-0 mt-3">
+          <span>Search Your</span> Word
+        </h1>
         <form className="d-flex justify-content-center" onSubmit={handleSubmit}>
           <div className="search__input">
             <input placeholder="Enter your Word" onChange={handleSearch} />
