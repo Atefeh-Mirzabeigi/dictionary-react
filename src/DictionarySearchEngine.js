e8a9ee41bdf15bd979ca755ab2cb9c156d3b5ba0
Atefeh <at.mirzabeigi@gmail.com>
2021-10-23 13:59:01 +0330

    create search functions

--- src/DictionarySearchEngine.js
+++ src/DictionarySearchEngine.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchEngine.css";
 export default function DictionarySearchEngine() {
+  const [searchWord, setSearchWord] = useState(null);
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(searchWord);
+  }
+  function handleSearch(event) {
+    setSearchWord(event.target.value);
+  }
   return (
     <div className="SearchEngine container-fluid">
       <h1 className="search__heading text-center mb-0 mt-3">
         Search Your Word
       </h1>
-      <div className="search d-flex justify-content-center mt-2">
-        <input className="search__input px-3" placeholder="Enter your Word" />
-        <button className="search__btn btn">
-          {" "}
-          <i className="bi bi-search"></i>{" "}
-        </button>
+      <div className="search">
+        <form className="d-flex justify-content-center" onSubmit={handleSubmit}>
+          <div className="search__input">
+            <input placeholder="Enter your Word" onChange={handleSearch} />
+          </div>
+          <div className="search__btn">
+            <button>
+              {" "}
+              <i className="bi bi-search"></i>{" "}
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
